0f46d9e7e19284900ba2228ec760e3a8ccc5b5ca
xufei-96 <857982765@qq.com>
2020-10-27 17:06:39 +0800

    没什么想说的

--- src/main/java/com/offcn/springsessiondemo/SpringsessiondemoApplication.java
+++ src/main/java/com/offcn/springsessiondemo/SpringsessiondemoApplication.java
@@ -9,6 +9,7 @@ public class SpringsessiondemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringsessiondemoApplication.class, args);
         System.out.println("这个项目已经上传过git");
+        System.out.println("正在创建一个分支");
     }
 
 }
