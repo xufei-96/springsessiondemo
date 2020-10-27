4630c6766fd0605a19f75d85b29f0b2ecaf631e0
xufei-96 <857982765@qq.com>
2020-10-27 16:35:17 +0800

    Initial commit

---
+++ src/main/java/com/offcn/springsessiondemo/SpringsessiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.offcn.springsessiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsessiondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsessiondemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/offcn/springsessiondemo/SpringsessiondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offcn.springsessiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsessiondemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
