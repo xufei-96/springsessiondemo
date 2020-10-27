0a751ea07d4da672f0d57244fb62942acfe1c367
xufei-96 <857982765@qq.com>
2020-10-27 17:12:18 +0800

    这是第二个分支

--- src/main/java/com/offcn/springsessiondemo/SpringsessiondemoApplication.java
+++ src/main/java/com/offcn/springsessiondemo/SpringsessiondemoApplication.java
@@ -10,6 +10,7 @@ public class SpringsessiondemoApplication {
         SpringApplication.run(SpringsessiondemoApplication.class, args);
         System.out.println("这个项目已经上传过git");
         System.out.println("正在创建一个分支");
+        System.out.println("创建第二个分支");
     }
 
 }
